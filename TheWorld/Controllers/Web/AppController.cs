2d488794f330006873d6457c998530322af0bfb1
Tony Cooke <tonyisaway@hotmail.com>
2016-12-31 14:41:09 +0000

    MVC 6

---
+++ TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,56 @@
+﻿namespace TheWorld.Controllers.Web
+{
+    using System;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+
+    using TheWorld.Services;
+    using TheWorld.ViewModels;
+
+    public class AppController : Controller
+    {
+        private IMailService mailService;
+
+        private IConfigurationRoot config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            this.mailService = mailService;
+            this.config = config;
+        }
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        public IActionResult Contact()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (model.Email.Contains("aol.com"))
+            {
+                this.ModelState.AddModelError(string.Empty, "We don't support AOL addresses");
+            }
+
+            if (this.ModelState.IsValid)
+            {
+                this.mailService.SendMail(this.config["MailSettings:ToAddress"], model.Email, "From the World.", model.Message);
+            }
+
+            this.ViewBag.UserMessage = "Message Sent";
+            this.ModelState.Clear();
+
+            return this.View();
+        }
+
+        public IActionResult About()
+        {
+            return this.View();
+        }
+    }
+}
--- TheWorld/Startup.cs
+++ TheWorld/Startup.cs
@@ -3,15 +3,42 @@
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
 
+    using TheWorld.Services;
+
     public class Startup
     {
+        private readonly IHostingEnvironment env;
+
+        private IConfigurationRoot config;
+
+        public Startup(IHostingEnvironment env)
+        {
+            this.env = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(this.env.ContentRootPath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+            this.config = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(this.config);
+
+            if (this.env.IsEnvironment("Development") || this.env.IsEnvironment("Testing"))
+            {
+                services.AddScoped<IMailService, DebugMailService>();
+            }
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -19,13 +46,21 @@
         {
             loggerFactory.AddConsole();
 
-            if (env.IsDevelopment())
+            if (this.env.IsEnvironment("Development"))
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseDefaultFiles();
             app.UseStaticFiles();
+
+            app.UseMvc(
+                config =>
+                    {
+                        config.MapRoute(
+                            name: "Default",
+                            template: "{controller}/{action}/{id?}",
+                            defaults: new { controller = "App", action = "Index" });
+                    }); 
         }
     }
 }
